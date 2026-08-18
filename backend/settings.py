f30eeee8c83aba418222167f88e42d258e7cfb07
Hasna Noushad <hasnaelinjulliparambil@gmail.com>
2026-08-18 13:00:45 +0530

    Use environment secret key

--- backend/settings.py
+++ backend/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nvmfapprc^q3tqxe$a$k3j&ls!m@r$yl0as8h*oqluw)f2md$2'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
 
