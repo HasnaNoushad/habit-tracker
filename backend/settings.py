d123e2b0a6b0e430778aba5ce485de3f0d5924c2
Hasna Noushad <hasnaelinjulliparambil@gmail.com>
2026-08-18 13:10:46 +0530

    Fix production settings import

--- backend/settings.py
+++ backend/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
+
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
